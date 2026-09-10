73578804c7c4efd265c7cb24e614f05248fe66ba
Богдан Гайдук <FedoTot1327@yandex.ru>
2026-09-10 16:45:16 +0300

    Expose visual derivative primitives

--- src/stroy_snab/anonymization/__init__.py
+++ src/stroy_snab/anonymization/__init__.py
@@ -3,13 +3,17 @@
 from .ids import neutral_id, validate_neutral_id
 from .leakcheck import LeakFinding, LeakReport, scan_path, scan_text
 from .manifest import build_public_manifest, validate_public_manifest
+from .visual_derivative import NormalizedRedactionBox, rebuild_image_to_png, render_pdf_to_pngs
 from .xlsx_rebuild import write_sanitized_workbook
 
 __all__ = [
     "LeakFinding",
     "LeakReport",
+    "NormalizedRedactionBox",
     "build_public_manifest",
     "neutral_id",
+    "rebuild_image_to_png",
+    "render_pdf_to_pngs",
     "scan_path",
     "scan_text",
     "validate_neutral_id",
